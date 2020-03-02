320aaf3f35b1ecfa16c6de04debeff571269bad8
Steven Topazio <sat@northpointprinting.com>
2020-03-02 16:30:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,12 +1,12 @@
 WIN_COMBINATIONS = [
-  [0,1,2], # top row 
-  [3,4,5], # middle row
-  [6,7,8], # bottom row 
-  [0,3,6], # first column 
-  [1,4,7], # middle column
-  [2,5,8], # last column
-  [0,4,8], # diagonal1 
-  [2,4,6] # diagonal2 
+  [0,1,2],  
+  [3,4,5], 
+  [6,7,8],  
+  [0,3,6], 
+  [1,4,7], 
+  [2,5,8], 
+  [0,4,8], 
+  [2,4,6] 
   ]
 
 def display_board(board)
