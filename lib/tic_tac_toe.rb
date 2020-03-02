2b10ea7b44ab8a3a1c44382873bb1c88404d458f
Steven Topazio <sat@northpointprinting.com>
2020-03-02 15:10:01 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,12 @@
 def WIN_COMBINATIONS = [
-  [0,1,2], #top row 
-  [3,4,5] #middle row
-  [6,7,8] #bottom row 
-  [0,3,6] #first column ]
+  [0,1,2], # top row 
+  [3,4,5] # middle row
+  [6,7,8] # bottom row 
+  [0,3,6] # first column 
+  [1,4,7] # middle column
+  [2,5,8] # last column
+  [0,4,8] # diagonal1 
+  [2,4,6] # diagonal2 
+  ]
+  
+  
